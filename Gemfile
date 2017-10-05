fa85ac106dfada93e74aada694bc5a4b8896e4f2
ryanmonro <ryan@ryanmonro.info>
2017-10-05 15:07:34 +1100

    Gemfile.lock added for Heroku

--- Gemfile
+++ Gemfile
@@ -1,3 +1,8 @@
 source 'https://rubygems.org'
 
 gem 'sinatra'
+gem 'pg'
+gem 'activerecord'
+gem 'bcrypt'
+gem 'pry'
+gem 'httparty'
--- main.rb
+++ main.rb
@@ -1,6 +1,7 @@
 
 require 'sinatra'
-require 'sinatra/reloader'
+require 'active_record'
+# require 'sinatra/reloader'
 require_relative 'db_config'
 require_relative 'models/user'
 require_relative 'models/item'
